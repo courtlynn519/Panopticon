c3b2a60fb581c76db357104ef1a575ee0e16847b
JonathanMcMahan <72231694+JonathanMcMahan@users.noreply.github.com>
2020-10-04 11:03:25 -0500

    Restart Game

--- TAGD Panopticon/Assets/Scripts/PlayerCollision.cs
+++ TAGD Panopticon/Assets/Scripts/PlayerCollision.cs
@@ -5,8 +5,10 @@ public class PlayerCollision : MonoBehaviour
 
     public PlayerMovement movement;
     void OnCollisionEnter (Collision collisionInfo) {
+
         if (collisionInfo.collider.tag == "Obstacle"){
             movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 
